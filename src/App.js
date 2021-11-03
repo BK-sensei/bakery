24ef543ba54a83a9d47e599272c929c8a63e7833
becca <rebecca.kaci@gmail.com>
2021-11-03 15:47:02 +0100

    définition du state inital dans 'App.js'

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from './components/Button.js'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      activeTab: "add",
+      items: []
+    }
+  }
+
+  
+
   render() {
     return (
       <div className="d-flex">
